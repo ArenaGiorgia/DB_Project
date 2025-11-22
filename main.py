e4ff3368562af3b584663ed4cd857e2e7b3612cc
ArenaGiorgia <arena.giorgia@studium.unict.it>
2025-11-22 15:23:54 +0100

    aggiunta file iniziali

--- main.py
+++ main.py
@@ -2,7 +2,10 @@
 
 # Press Maiusc+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+# from opensky_api import OpenSkyApi
+# api = OpenSkyApi()
+# s = api.get_states()
+# print(s)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
